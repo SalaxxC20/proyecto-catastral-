82bdd5d370f306584a2619989eb0c25e7b90d27f
JulianC20 <salazardix321@gamil.com>
2024-02-12 15:46:03 -0500

    v0

---
+++ app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AboutPage() {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, similique temporibus. Accusamus cupiditate sunt sed sequi amet minus eum sit repudiandae eveniet blanditiis, incidunt aliquam ipsum quidem facere fugiat! Itaque.</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ app/services/page.jsx
@@ -0,0 +1,10 @@
+function Services() {
+  return (
+    <div>
+        <h1>Services</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem assumenda perferendis mollitia tempore fugit voluptatem voluptatibus. Incidunt eos magnam ipsum autem iusto esse rem, ex temporibus aliquid, quos possimus non.</p>
+    </div>
+  )
+}
+
+export default Services
